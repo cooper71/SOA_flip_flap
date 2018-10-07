7a75fd29f6e7e89602955bc790c2d15ffab5afec
coolly <s107078515@m107.nthu.edu.tw>
2018-10-07 19:27:38 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,12 +3,25 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data = []
+    rows = tsv.split("\n").map { |line| line.split("\t") }
+    keys = rows.first
+    rows[1..-1].map { |values| @data << keys.zip(values).to_h }
+    @data
+  rescue StandardError => e
+    raise e.message
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    @tsv = ''
+    @data.each_with_index do |hash_item, index|
+      @tsv += hash_item.keys.join("\t") + "\n" if index.zero?
+      @tsv += hash_item.values.join("\t") + "\n"
+    end
+    @tsv
+  rescue StandardError => e
+    raise e.message
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  rescue StandardError => e
+  raise e.message
+end
+
+def to_yaml
+    @data = @data.to_yaml
+  rescue StandardError => e
+    raise e.message
+  end
+  end
